787b20b2cbe45530fc4d9b6c40cd31982ab77b53
test <test>
2025-03-04 12:38:54 +0700

    test

---
+++ bank.py
@@ -0,0 +1,13 @@
+def bank():
+    x = input("Приветствие : ")
+
+    if x.startswith("здравствуйте"):
+        print("$0")
+
+    elif x.startswith("з"):
+     print("$20")
+
+    else:
+     print("$100")
+
+bank()
